1e1a786edc510851a49f7b4c6830a8df79fb3091
Viet <chotrongnha@gmail.com>
2023-06-02 21:33:30 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Player from './components/Player';
 import Footer from './components/Footer';
+import Main from './components/Main';
 function App() {
   return (
     <div className='App'>
       <Navigation/>
-       <Player/>
+      <Main/>
        <Footer/>
     </div>
   );
